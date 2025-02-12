749475304f25c455f6b48b25d5feeeff66c99b76
dizzysch <victorwilbert@gmail.com>
2025-02-11 22:17:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using ObjectDetection.YoloParser;
+using ObjectDetection.DataStructures;
+using ObjectDetection;
+using Microsoft.ML;
+
+var assetsRelativePath = @"../../../assets";
+string assetsPath = GetAbsolutePath(assetsRelativePath);
+var modelFilePath = Path.Combine(assetsPath, "Model", "TinyYolo2_model.onnx");
+var imagesFolder = Path.Combine(assetsPath, "images");
+var outputFolder = Path.Combine(assetsPath, "images", "output");
+MLContext mlContext = new MLContext();
+
+
+string GetAbsolutePath(string relativePath)
+{
+    FileInfo _dataRoot = new FileInfo(typeof(Program).Assembly.Location);
+    string assemblyFolderPath = _dataRoot.Directory.FullName;
+
+    string fullPath = Path.Combine(assemblyFolderPath, relativePath);
+
+    return fullPath;
+}
---
+++ YoloParser/YoloBoundingBox.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectDetection.YoloParser
+
+{
+    public class BoundingBoxDimensions : DimensionsBase { }
+
+    public class YoloBoundingBox
+    {
+        public BoundingBoxDimensions Dimensions { get; set; }
+
+        public string Label { get; set; }
+
+        public float Confidence { get; set; }
+
+        public RectangleF Rect
+        {
+            get { return new RectangleF(Dimensions.X, Dimensions.Y, Dimensions.Width, Dimensions.Height); }
+        }
+
+        public Color BoxColor { get; set; }
+    }
+}
